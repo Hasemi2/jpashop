e4e32ce6c361b07a35c60b8f0169ddde813b2ac8
하세미 <h171754@gmail.com>
2021-01-19 22:10:57 +0900

    상품 도메인 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,29 @@ public class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //데이터를 가지고 있는 도메인에 핵심 비즈니스 로직을 관리하는 것이 좋다.
+    //setter를 무분별하게 사용하여 서비스레이어에서 계산x
+    //==비즈니스 로직==//
+
+    /**
+     * 재고 증가
+     *
+     * @param stockQuantity
+     */
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    /**
+     * 재고 감소
+     *
+     * @param stockQuantity
+     */
+    public void removeStock(int stockQuantity) {
+        int restStock = this.stockQuantity - stockQuantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock ");
+        }
+        this.stockQuantity = restStock;
+    }
 }
